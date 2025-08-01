c39f18ac6a3945916c8d75015873f0d563dbf054
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-08-01 21:15:53 +0100

    feat: add instruction for keys in WindowEvent

--- src/core/window/window.rs
+++ src/core/window/window.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use pollster::block_on;
 use crate::core::render::state::State;
-use winit::{application::ApplicationHandler, event::WindowEvent, window::Window};
+use winit::{application::ApplicationHandler, event::{KeyEvent, WindowEvent}, keyboard::PhysicalKey, window::Window};
 
 pub struct App {
     state: Option<State>,
@@ -30,23 +30,43 @@ impl ApplicationHandler for App {
     }
 
     fn window_event(
-            &mut self,
-            event_loop: &winit::event_loop::ActiveEventLoop,
-            window_id: winit::window::WindowId,
-            event: WindowEvent,
-        ) {
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
         if Some(window_id) != self.window.as_ref().map(|w| w.id()) {
             return;
         }
+        
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
 
         match event {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             },
+            WindowEvent::KeyboardInput { 
+                device_id: _, 
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    }, 
+                is_synthetic: _ 
+            } => state.handle_key(event_loop, code, key_state.is_pressed()),
             WindowEvent::RedrawRequested => {
+                match state.render() {
+                    Ok(_) => {}
+                    Err(e) => {
+                        eprintln!("Unable to render {}", e);
+                    }
+                }
                 self.window.as_ref().unwrap().request_redraw();
             }
             _ => ()
-        }
-    }
+        }}
 }
