4169b3fe4e4a18a1a5ad74237fd4473855ca7069
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-08-01 19:21:43 +0100

    style: format indentation of state.rs code

--- src/core/render/state.rs
+++ src/core/render/state.rs
@@ -2,61 +2,62 @@ use std::sync::Arc;
 use winit::window::Window;
 
 pub struct State {
-  surface: wgpu::Surface<'static>,
-  device: wgpu::Device,
   queue: wgpu::Queue,
-  config: wgpu::SurfaceConfiguration,
   window: Arc<Window>,
+  device: wgpu::Device,
+  surface: wgpu::Surface<'static>,
+  config: wgpu::SurfaceConfiguration,
 }
 
 impl State {
-    pub async fn new(window: Arc<Window>) -> Self {
-      let size = window.inner_size();
-      
-      let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-        backends: wgpu::Backends::all(),
-        ..Default::default()
-      });
+  pub async fn new(window: Arc<Window>) -> Self {
+    let size = window.inner_size();
+    
+    let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+      backends: wgpu::Backends::all(),
+      ..Default::default()
+    });
 
-      let surface = instance.create_surface(window.clone()).unwrap();
+    let surface = instance.create_surface(window.clone()).unwrap();
+    
+    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
+      power_preference: wgpu::PowerPreference::HighPerformance,
+      force_fallback_adapter: false,
+      compatible_surface: Some(&surface)
+    })
+    .await
+    .unwrap();
 
-      let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
-        power_preference: wgpu::PowerPreference::HighPerformance,
-        force_fallback_adapter: false,
-        compatible_surface: Some(&surface)
-      })
-      .await
-      .unwrap();
+    let (device, queue) = adapter.request_device(
+      &wgpu::DeviceDescriptor {
+        label:None,
+        trace: wgpu::Trace::Off,
+        memory_hints: Default::default(),
+        required_limits: wgpu::Limits::defaults(),
+        required_features: wgpu::Features::default(),
+      }
+    )
+    .await
+    .unwrap();
 
-      let (device, queue) = adapter.request_device(
-        &wgpu::DeviceDescriptor {
-          label:None,
-          trace: wgpu::Trace::Off,
-          memory_hints: Default::default(),
-          required_limits: wgpu::Limits::defaults(),
-          required_features: wgpu::Features::default(),
-        }
-      )
-      .await
-      .unwrap();
+    let surface_caps = surface.get_capabilities(&adapter);
+    let surface_format = surface_caps.formats
+      .iter().find(|f| f.is_srgb())
+      .copied().unwrap_or(surface_caps.formats[0]);
 
-      let surface_caps = surface.get_capabilities(&adapter);
-      let surface_format = surface_caps.formats
-        .iter().find(|f| f.is_srgb())
-        .copied().unwrap_or(surface_caps.formats[0]);
+    let config = wgpu::SurfaceConfiguration {
+      usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+      format: surface_format,
+      width: size.width,
+      height: size.height,
+      present_mode: surface_caps.present_modes[0],
+      alpha_mode: surface_caps.alpha_modes[0],
+      view_formats: vec![],
+      desired_maximum_frame_latency: 2
+    };
 
-      let config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: surface_format,
-        width: size.width,
-        height: size.height,
-        present_mode: surface_caps.present_modes[0],
-        alpha_mode: surface_caps.alpha_modes[0],
-        view_formats: vec![],
-        desired_maximum_frame_latency: 2
-      };
-      surface.configure(&device, &config);
+    surface.configure(&device, &config);
 
-      Self { queue, window, device: device, surface, config }
-    }
+    Self { queue, window, device: device, surface, config }
+  }
 }
