658429363ac662e3220e52e97b74950ec8e3c355
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-08-01 19:33:12 +0100

    feat: add wgpu render_pipeline settings

--- src/core/render/state.rs
+++ src/core/render/state.rs
@@ -7,6 +7,7 @@ pub struct State {
   device: wgpu::Device,
   surface: wgpu::Surface<'static>,
   config: wgpu::SurfaceConfiguration,
+  render_pipeline: wgpu::RenderPipeline
 }
 
 impl State {
@@ -58,6 +59,55 @@ impl State {
 
     surface.configure(&device, &config);
 
-    Self { queue, window, device: device, surface, config }
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+      label: Some("Shader"),
+      source: wgpu::ShaderSource::Wgsl(include_str!("../shaders/shader.wgsl").into()),
+    });
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+      label: Some("Render Pipeline Layout"),
+      bind_group_layouts: &[],
+      push_constant_ranges: &[]
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+      label: Some("Render Pipeline"),
+      layout: Some(&render_pipeline_layout),
+      vertex: wgpu::VertexState {
+        module: &shader,
+        entry_point: Some("vs_main"),
+        buffers: &[],
+        compilation_options: wgpu::PipelineCompilationOptions::default()
+      },
+      fragment: Some(wgpu::FragmentState {
+        module: &shader,
+        entry_point: Some("fs_main"),
+        targets: &[Some(wgpu::ColorTargetState {
+          format: config.format,
+          blend: Some(wgpu::BlendState::REPLACE),
+          write_mask: wgpu::ColorWrites::ALL
+        })],
+        compilation_options: wgpu::PipelineCompilationOptions::default()
+      }),
+      primitive: wgpu::PrimitiveState {
+        topology: wgpu::PrimitiveTopology::TriangleList,
+        strip_index_format: None,
+        front_face: wgpu::FrontFace::Ccw,
+        cull_mode: Some(wgpu::Face::Back),
+        polygon_mode: wgpu::PolygonMode::Fill,
+        unclipped_depth: false,
+        conservative: false
+      },
+      depth_stencil: None,
+      multisample: wgpu::MultisampleState {
+        count: 1,
+        mask: !0,
+        alpha_to_coverage_enabled: false
+      },
+      multiview: None,
+      cache: None
+    });
+
+    Self { queue, window, device: device, surface, config, render_pipeline }
   }
 }
