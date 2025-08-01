e70b11c8062448650e37ac58062391d8cce5a1d0
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-08-01 20:49:40 +0100

    feat: create the function of rendering and capturing keys

--- src/core/render/state.rs
+++ src/core/render/state.rs
@@ -1,5 +1,5 @@
 use std::sync::Arc;
-use winit::window::Window;
+use winit::{keyboard::KeyCode, window::Window};
 
 pub struct State {
   queue: wgpu::Queue,
@@ -109,5 +109,51 @@ impl State {
     });
 
     Self { queue, window, device: device, surface, config, render_pipeline }
+  }
+
+  pub fn handle_key(&self, event_loop: &winit::event_loop::ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+    match (code, is_pressed) {
+      (KeyCode::Escape, true) => event_loop.exit(),
+      (KeyCode::KeyW, true) => println!("W"),
+      (KeyCode::KeyA, true) => println!("A"),
+      (KeyCode::KeyS, true) => println!("S"),
+      (KeyCode::KeyD, true) => println!("D"),
+      (KeyCode::KeyF, true) => println!("F"),
+      _ => {}
+    }
+  }
+  
+  pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+    self.window.request_redraw();
+
+    let output = self.surface.get_current_texture()?;
+    let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+    let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+      label: Some("Render Encoder"),
+    });
+
+    {
+      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: Some("Render Pass"),
+        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+          view: &view,
+          resolve_target:None,
+          depth_slice: None,
+          ops: wgpu::Operations {
+            load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.2, g: 0.2, b: 0.3, a: 1.0 }),
+            store: wgpu::StoreOp::Store
+          },
+        })],
+        depth_stencil_attachment: None,
+        occlusion_query_set: None,
+        timestamp_writes: None
+      });
+    }
+
+    self.queue.submit(std::iter::once(encoder.finish()));
+    output.present();
+
+    Ok(())
   }
 }
